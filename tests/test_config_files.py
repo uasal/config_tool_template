f871faccbac27e8d4cd701c2cc94bb5443147157
Patrick Ingraham <pingraham@arizona.edu>
2025-02-05 22:07:59 -0700

    example folder layout

--- tests/test_config_files.py
+++ tests/test_config_files.py
@@ -1,31 +1,20 @@
 import pathlib
 import unittest
 import tomlkit
+from config_pearl import tests
+import astropy
 
 
 class TestConfigs(TestCase):
     "Tests for psd_utils."
 
-    def test_ETC_WCC(self):
-        # Go and read in each config file
-
-        # Fail test on _baseline
-
-        # throw warning on customs
-
+    def test_tomls(self):
+        # Go and read in each config file and check for syntax errors
+        files = *.toml
+        tests.syntax(files)
         # Also check astropy unit compatibility
-
+        tests.astropy_units(files)
         pass
 
- 
-    def test_ETC_ESC(self):
-        # Go and read in each config file
 
-        # Fail test on _baseline
-
-        # throw warning on customs
-
-        # Also check astropy unit compatibility
-
-        pass
     
